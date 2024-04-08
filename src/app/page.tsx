84599a67fd31e2c9d8e5d96f4bcb9d0d443f5ee8
Abid Famasya <abid.famasya@gmail.com>
2024-04-09 00:13:11 +0700

    fix: production build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
 'use client'
 
+import { familyTree } from '@/data';
 import { FamilyNode } from '@/types';
 import dynamic from 'next/dynamic';
 import { useState } from "react";
 
 const FamilyTree = dynamic(() => import('@/components/family-tree'), { ssr: false });
 
-export const familyTree = [{
-  name: 'Ayah',
-  spouse: 'Ibu',
-  id: 'id',
-}, {
-  name: 'Anak 1',
-  id: 'id1',
-  parentId: 'id'
-}, {
-  name: 'Anak 2',
-  id: 'id2',
-  parentId: 'id'
-}, {
-  name: 'Cucu 1',
-  id: 'id3',
-  parentId: 'id2'
-}];
 export default function Home() {
   const [data, setData] = useState<FamilyNode[]>(familyTree);
 
---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const familyTree = [{
+  name: 'Ayah',
+  spouse: 'Ibu',
+  id: 'id',
+}, {
+  name: 'Anak 1',
+  id: 'id1',
+  parentId: 'id'
+}, {
+  name: 'Anak 2',
+  id: 'id2',
+  parentId: 'id'
+}, {
+  name: 'Cucu 1',
+  id: 'id3',
+  parentId: 'id2'
+}];
